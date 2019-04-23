005acaa161a0f68814f619807387cd24824f1a00
c68559 <c68559@gmail.com>
2019-04-23 21:26:29 +0300

    add iscsi py

---
+++ clean-iscsi-session.py
@@ -0,0 +1,16 @@
+import subprocess
+import os
+from pprint import pprint as pp
+cmd = ['/usr/sbin/iscsiadm', '-m', 'session']
+p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+o, e = p.communicate()
+SESS = [ [ s.split()[2].split(',')[0], s.split()[3]] for s in o.split('\n') if s.__contains__('tcp') ]
+for c in SESS:
+  cmdline =  'iscsiadm --m node -T ' + c[1] + " --portal " + c[0] + ' -u'
+  pp(cmdline)
+  os.system(cmdline)
+  cmdline =  'iscsiadm --m node -T ' + c[1] + " --portal " + c[0] + ' -o delete'
+  pp(cmdline)
+  os.system(cmdline)
+ 
+
