17727cc6fd869abe0dc1395020858d3127a4995c
c68559 <c68559@gmail.com>
2019-04-23 21:40:34 +0300

    add this file

--- find_non-consecutive_timestamp_from_log.py
+++ find_non-consecutive_timestamp_from_log.py
@@ -3,7 +3,7 @@ from pprint import pprint as pp
 dd = []
 logfile = "perf.log"
 KeyWords = "execution of poll_system"
-with open(logfile,'r') as file:
+with open(logfile, 'r') as file:
     for line in file:
         if line.__contains__(KeyWords):
             tt = line[0:16]
@@ -15,8 +15,8 @@ with open(logfile,'r') as file:
                 pass
 
 
-TimeStamp = [ datetime.strptime(d, "%Y-%m-%d %H:%M") for d in dd ]
+TimeStamp = [datetime.strptime(d, "%Y-%m-%d %H:%M") for d in dd]
 GapAllowed = 1
-for i in range(1,len(TimeStamp)):
+for i in range(1, len(TimeStamp)):
     diff = (TimeStamp[i] - TimeStamp[i-1]).seconds/60
-    if diff != GapAllowed: print("line{0}: {1} ~ {2}".format(i-1,TimeStamp[i-1],TimeStamp[i])) 
+    if diff != GapAllowed: print("line{0}: {1} ~ {2}".format(i-1, TimeStamp[i-1], TimeStamp[i])) 
