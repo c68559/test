fd781ca4ea582e53fda4cb2ec4aeac35b8d1dcca
c68559 <c68559@gmail.com>
2019-04-23 22:41:45 +0300

    master changed

--- clean-iscsi-session.py
+++ clean-iscsi-session.py
@@ -5,6 +5,8 @@ cmd = ['/usr/sbin/iscsiadm', '-m', 'session']
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 o, e = p.communicate()
 SESS = [ [ s.split()[2].split(',')[0], s.split()[3]] for s in o.split('\n') if s.__contains__('tcp') ]
+
+
 for c in SESS:
   cmdline =  'iscsiadm --m node -T ' + c[1] + " --portal " + c[0] + ' -u'
   pp(cmdline)
